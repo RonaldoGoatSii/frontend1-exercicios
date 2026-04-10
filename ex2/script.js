50000ef85f9c7bf08304e491ea134f6bd7c323a8
RonaldoGoatSii <tiagolinsbrasileiro@gmail.com>
2026-04-10 11:07:31 +0100

    feat: making some changes on key and values for test, correcting the indentation

--- ex2/script.js
+++ ex2/script.js
@@ -10,7 +10,7 @@ formElement.addEventListener("submit", function(event) { // "submite" é o tipo 
 
     user.Nome = input.value; // aceder ao ao texto que o utilizador inseriu no input
 
-    localStorage.setItem("form", JSON.stringify(user)); // guardar o objeto (form) na localStorage, e o JSON.stringify para converter o objeto numa string 
+    localStorage.setItem("user", JSON.stringify(user)); // guardar o objeto (form) na localStorage, e o JSON.stringify para converter o objeto numa string 
 });
 
 let form1 = JSON.parse(localStorage.getItem("user")); // buscar a string guardada com a chave "form", e o JSON.parse para converter a string de volta para objeto
@@ -21,32 +21,32 @@ if (form1) {
 
 
 let button = document.getElementById("themeBtn");
-        let texto = document.getElementById("temaTexto");
+let texto = document.getElementById("temaTexto");
 
-        // carregar tema guardado
-        let temaGuardado = localStorage.getItem("tema");
+// carregar tema guardado
+let temaGuardado = localStorage.getItem("tema");
 
-        if (temaGuardado) {
-            document.body.className = temaGuardado;
-            texto.textContent = "Tema atual: " + temaGuardado;
-        } else {
-            document.body.className = "light";
-            texto.textContent = "Tema atual: light";
+if (temaGuardado) {
+    document.body.className = temaGuardado;
+    texto.textContent = "Tema atual: " + temaGuardado;
+    } else {
+        document.body.className = "light";
+        texto.textContent = "Tema atual: light";
         }
 
-        button.addEventListener("click", function() {
+button.addEventListener("click", function() {
 
-            let temaAtual = document.body.classList.contains("light") ? "light" : "dark";
-            let novoTema = temaAtual === "light" ? "dark" : "light";
+    let temaAtual = document.body.classList.contains("light") ? "light" : "dark";
+    let novoTema = temaAtual === "light" ? "dark" : "light";
 
-            document.body.className = novoTema;
+    document.body.className = novoTema;
 
             // guardar no localStorage
-            localStorage.setItem("tema", novoTema);
+    localStorage.setItem("tema", novoTema);
 
             // mostrar no ecrã
-            texto.textContent = "Tema atual: " + novoTema;
+    texto.textContent = "Tema atual: " + novoTema;
 
             // (opcional) ver no console
-            console.log(novoTema);
-        });
+    console.log(novoTema);
+});
