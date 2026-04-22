25e35172f92b813c0bafe10b6de4027e83f6139c
RonaldoGoatSii <172612901+RonaldoGoatSii@users.noreply.github.com>
2026-04-22 21:57:10 +0100

    feat: Todlingo site

---
+++ final/script.js
@@ -0,0 +1,12 @@
+let formElement = document.querySelector("form");
+let input = document.querySelector("input");
+
+
+formElement.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    localStorage.setItem("user",  ); 
+});
+
+
+
